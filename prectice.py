be4bb33830fd1bd640ad031bc5f10d2303aa0dd0
nishatdbdlt <nishat7453@gmail.com>
2025-11-04 21:36:59 +0600

    working

--- prectice.py
+++ prectice.py
@@ -154,3 +154,13 @@ def finonacii(n):
         a,b = b , a + b
     return result
 print(finonacii(6))
+
+
+def finonacci(s):
+    a,b =0,1
+    result =[]
+    for _ in range(s):
+        result.append(a)
+        a,b = b ,a + b
+    return result
+print(finonacii(6))
