459a3b7e3709da546674f8664b6dd2e908d246d1
nishatdbdlt <nishat7453@gmail.com>
2025-11-04 18:04:26 +0600

    working

--- problem_solving.py
+++ problem_solving.py
@@ -1,4 +1,5 @@
 from functools import total_ordering
+from os import sendfile
 
 print('nishat')
 
@@ -45,3 +46,52 @@ def word_frequency(sentance):
         freq[w] = freq.get(w,0) +1
     return freq
 print(word_frequency("nishat is a , good boy boy"))
+
+
+def word_count(sentance):
+    word=sentance.split()
+    ferq={}
+    for w in word:
+        ferq[w] = ferq.get(w,0) +1
+    return ferq
+print(word_count('nishat nishat '))
+
+
+def even_num(num):
+    total =0
+    for n in num:
+        if n % 2 ==0:
+            total +=n
+    return total
+print(even_num([2,3,4,5,6,7,8,9]))
+
+
+def ood_num(num):
+    total =0
+    for n in num:
+        if n % 2 !=0:
+            total +=n
+    return total
+print(ood_num([3,4,5,6,7,8,9]))
+
+
+def revesre_string(s):
+    return s[:: -1]
+print(revesre_string('nishat'))
+
+def word_counter(sentence):
+    word = sentence .split()
+    freq ={}
+    for w in word:
+        freq[w] = freq.get(w ,0) + 1
+    return freq
+print(word_counter("nishat nisaht bangladesh bangladesh nishat nishat "))
+
+
+def word_counter(sentance):
+    word = sentance.split()
+    freq={}
+    for w in word:
+        freq[w] =freq.get(w,0) + 1
+    return freq
+print(word_counter("nishat nishat nishat bangladesh "))
