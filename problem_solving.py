51f5450819befc8ca78a49261706d265b5f2bbb1
nishatdbdlt <nishat7453@gmail.com>
2025-11-04 18:22:22 +0600

    be continue

--- problem_solving.py
+++ problem_solving.py
@@ -95,3 +95,30 @@ def word_counter(sentance):
         freq[w] =freq.get(w,0) + 1
     return freq
 print(word_counter("nishat nishat nishat bangladesh "))
+
+def most_frequent(lst):
+    freq ={}
+    for i in lst:
+        freq[i] = freq.get(i ,0) + 1
+    return max(freq, key=freq.get)
+print(most_frequent([2,3,4,5,6,7,8,]))
+
+
+def most_letter(lst):
+    freq= {}
+    for i in lst:
+        freq[i] = freq.get(i,0) + 1
+    return max(freq,key=freq.get)
+print(most_letter([3,4,5,6,7,8,9,33,33,33]))
+#palindrome
+def is_palindrome(s):
+    s = s.lower() .replace( " ", "")
+    return  s == s[:: -1]
+print(is_palindrome('madaM'))
+print(is_palindrome('nishat'))
+
+
+def is_palindrome(s):
+    s = s.lower() .replace("" , " ")
+    return s == s[: : -1]
+print(is_palindrome("Nishat"))
